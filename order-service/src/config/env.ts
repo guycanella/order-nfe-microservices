0dd3c039ba027a7a1cafe8b7b90f65635c665a49
Guilherme <guycanella@gmail.com>
2025-09-07 11:19:32 -0300

    add env.ts config file for order service

---
+++ order-service/src/config/env.ts
@@ -0,0 +1,10 @@
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+export const env = {
+    NODE_ENV: process.env.NODE_ENV || 'development',
+    PORT: Number(process.env.PORT) || 3001,
+    DATABASE_URL: process.env.DATABASE_URL || '',
+    RABBITMQ_URL: process.env.RABBITMQ_URL || '',
+}
