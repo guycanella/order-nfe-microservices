a8757c457471ba342b3d62391d92a086f64c6f98
Guilherme <guycanella@gmail.com>
2025-09-07 14:27:50 -0300

    create ErrorHandler class

---
+++ order-service/src/utils/ErrorHandler.ts
@@ -0,0 +1,33 @@
+import { FastifyReply } from "fastify"
+
+type ErrorHandlerType = {
+    error: unknown
+    origin: string
+    reply?: FastifyReply
+}
+
+export class ErrorHandler {
+    static handle({ error, origin, reply }: ErrorHandlerType) {
+        if (error instanceof Error) {
+            console.error(`Error origin: ${origin}`)
+            console.error(error.message)
+
+            if (!reply) return
+
+            reply.status(500).send({
+                error: "Internal Server Error",
+                message: error.message
+            })
+        } else {
+            console.error(`Error origin: ${origin}`)
+            console.error("Unknown error", error)
+
+            if (!reply) return
+
+            reply.status(500).send({
+                error: "Internal Server Error",
+                message: "Unexpected error"
+            })
+        }
+    }
+}
