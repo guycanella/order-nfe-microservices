a435d6ef6a40caa2b9d83b99babf49bc8dbe1ab9
Guilherme <guycanella@gmail.com>
2025-09-07 11:35:21 -0300

    add connection and publish functions for RabbitMQ

---
+++ order-service/src/brokers/rabbitmq.ts
@@ -0,0 +1,21 @@
+import amqplib from 'amqplib';
+import { env } from '../config/env'
+
+let channel: amqplib.Channel | null = null
+
+export async function initRabbitMQ() {
+    const conn = await amqplib.connect(env.RABBITMQ_URL)
+    channel = await conn.createChannel()
+
+    console.log("🐇 Connected to RabbitMQ")
+}
+
+export async function publishEvent(queue: string, message: string) {
+    if (!channel) throw new Error("RabbitMQ not initialized")
+
+    await channel.assertQueue(queue, { durable: true })
+
+    channel.sendToQueue(queue, Buffer.from(JSON.stringify(message)), { persistent: true })
+
+    console.log(`📤 Event published -> ${queue}`, message)
+}
